6a8ceea41a1048cfcef2f4ec95302fbd264c537a
Marcelo Humerez <60332359+marcelohumerez@users.noreply.github.com>
2021-06-08 15:49:45 -0500

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>res.send('prueba de server NodeJS'));
+app.get('/', (req, res) =>res.send('prueba de server ---- NodeJS'));
 
 app.listen(3000);
 console.log('Server on port', 3000);
